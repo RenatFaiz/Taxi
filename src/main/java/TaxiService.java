e469cec9a763d59fd4ca41c5db4285411809aed1
RenatFaiz <renatf87@gmail.com>
2019-09-26 12:46:25 +0300

    first commit

---
+++ src/main/java/TaxiService.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TaxiService {
+
+    int seatCost = 60;
+    int priceForKm = 20;
+    int bonus = 5;
+    double totalCost;
+    int maxDiscount = 100;
+    int orderOnThousand = 1000;
+
+    void costCalculator() {
+
+        System.out.println("Добро пожаловать в службу заказа такси!\n" +
+                "Сколько километров Вам нужно проехать?");
+        Scanner in = new Scanner(System.in);
+        int distance = in.nextInt();
+        double costTravel = distance * priceForKm + seatCost;
+        double discountAmount = (costTravel / 100) * bonus;
+
+        if (costTravel < orderOnThousand) {
+            totalCost = costTravel;
+        } else if (costTravel > orderOnThousand && discountAmount <= maxDiscount) {
+            totalCost = costTravel - discountAmount;
+        } else {
+            totalCost = costTravel - maxDiscount;
+        }
+        System.out.println("Стоимость поездки будет равна: " + (int) totalCost + " руб.");
+    }
+
+}
